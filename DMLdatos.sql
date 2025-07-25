8c7eac1dc8db6989826aebfff26834605aaa9ce6
sergio <slievano212@gmail.com>
2025-07-25 17:56:55 -0500

    feat: :memo: Se añaden datos de Mascotas
    Se añaden datos de mascotas como atributos
    
    None

--- DMLdatos.sql
+++ DMLdatos.sql
@@ -6,3 +6,15 @@ INSERT INTO Dueños (cedula, nombre, telefono, direccion) VALUES
 ('100000004', 'Diana Torres', '3123456789', 'Calle 80 #50-10'),
 ('100000005', 'José Ramírez', '3004567890', 'Carrera 8 #12-45');
 
+-- Mascotas
+INSERT INTO Mascotas (ID_dueño, nombre_mascota, especie, raza, edad, sexo, vacunado) VALUES
+(1, 'Firulais', 'Perro', 'Labrador', 4, 'Macho', TRUE),
+(1, 'Misu', 'Gato', 'Persa', 2, 'Hembra', TRUE),
+(2, 'Rocky', 'Perro', 'Bulldog', 5, 'Macho', FALSE),
+(2, 'Luna', 'Gato', 'Siames', 1, 'Hembra', TRUE),
+(3, 'Max', 'Perro', 'Golden Retriever', 3, 'Macho', TRUE),
+(3, 'Nala', 'Gato', 'Maine Coon', 6, 'Hembra', FALSE),
+(4, 'Toby', 'Perro', 'Poodle', 2, 'Macho', TRUE),
+(4, 'Simba', 'Gato', 'Bengalí', 3, 'Macho', TRUE),
+(5, 'Kira', 'Perro', 'Dálmata', 1, 'Hembra', FALSE),
+(5, 'Milo', 'Gato', 'Sphynx', 4, 'Macho', TRUE);
