50e3dfff6ad5884e22846ee401749146780d94cc
sergio <slievano212@gmail.com>
2025-07-25 17:48:52 -0500

    feat: :tada: Creación de archivo de datos SQL
    Iniciación de datos SQL con datos de Dueños
    
    None

---
+++ DMLdatos.sql
@@ -0,0 +1,8 @@
+-- Dueños
+INSERT INTO Dueños (cedula, nombre, telefono, direccion) VALUES
+('100000001', 'Carlos Mendoza', '3001234567', 'Calle 10 #5-22'),
+('100000002', 'Laura García', '3109876543', 'Carrera 15 #45-12'),
+('100000003', 'Andrés Pérez', '3112233445', 'Av. 68 #22-30'),
+('100000004', 'Diana Torres', '3123456789', 'Calle 80 #50-10'),
+('100000005', 'José Ramírez', '3004567890', 'Carrera 8 #12-45');
+
